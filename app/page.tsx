5a6ab89ea33596216f8ffd67b208fab561ced02d
arhamkac <arhamkachhara@gmail.com>
2026-03-05 11:42:01 +0530

    Pushed AboutUs above gallery

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ export default function Page() {
       <ScrollHeader />
       <AppShell />
       <UnifiedParallaxSection />
-      <SalesLoungeGallery />
       <AboutSection />
+      <SalesLoungeGallery />
       <ServicesSection />
       <StatsSection />
       <TeamSection />
